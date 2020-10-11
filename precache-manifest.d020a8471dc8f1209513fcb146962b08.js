d60af0a57dafbd269e51c599d109c9507c1252f0
ccduisburg <cemilkabakci@gmail.com>
2020-10-11 18:03:41 +0200

    Updates

---
+++ precache-manifest.d020a8471dc8f1209513fcb146962b08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1d2f140712e3dba7e4389bd7409db6",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "b4e78729596b635c62e1",
+    "url": "/monster/static/css/main.dd869e17.chunk.css"
+  },
+  {
+    "revision": "70f2ad298bef0f2575a8",
+    "url": "/monster/static/js/2.1e719e16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.1e719e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e78729596b635c62e1",
+    "url": "/monster/static/js/main.eb1ca61c.chunk.js"
+  },
+  {
+    "revision": "1a053d53893bc93c935a",
+    "url": "/monster/static/js/runtime-main.72840dc5.js"
+  }
+]);
